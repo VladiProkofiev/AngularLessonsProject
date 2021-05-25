608967e96bf62f4c7a4fb5a74fe1773e728cb018
prokofev <prokofev@teleformis.ru>
2021-05-25 17:57:30 +0300

    Создание атрибутных директив

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ChildComponent } from './components/child/child.component';
 import { DataModule } from './data/data.module';
+import { BoldDirective } from './bold.directive';
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
+    ChildComponent,
+    BoldDirective
   ],
   imports: [
     BrowserModule,
