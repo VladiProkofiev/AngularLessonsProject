d5b35a983215722e16fb1a0c698786e4ab947476
prokofev <prokofev@teleformis.ru>
2021-05-26 10:37:25 +0300

    HostListener и HostBinding и свойство host обработчика событий

--- src/app/bold.directive.ts
+++ src/app/bold.directive.ts
@@ -1,12 +1,21 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
 
 @Directive({
-  selector: '[appBold]'
+  selector: '[appBold]',
+  host: {'(mouseenter)': 'onMouseEnter()'},
 })
 export class BoldDirective {
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { 
-    this.renderer.setStyle(this.elementRef.nativeElement, "font-weight", "bold")
+    this.renderer.setStyle(this.elementRef.nativeElement, "cursor", "pointer")
   }
-
+onMouseEnter() {
+  this.setFontWeight("bold");
+}
+@HostListener("mouseleave") onMouseLeave() {
+  this.setFontWeight("normal");
+}
+private setFontWeight(value: string) {
+  this.renderer.setStyle(this.elementRef.nativeElement, "font-weight", value);
+}
 }
